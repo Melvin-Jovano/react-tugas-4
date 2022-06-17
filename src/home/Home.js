14c4f8896939f7957812ba4b7d0ea2c55e67f1f3
Melvin-Jovano <melvinjovano2@gmail.com>
2022-06-17 23:41:59 +0700

    change icons

---
+++ src/home/Home.js
@@ -0,0 +1,12 @@
+// import 'https://cdn.tailwindcss.com';
+
+function Home() {
+  return (
+    <div className='text-black'>
+      asdbahsdbajbdhabdajbdabhdhadsas
+      asdajbdhahds
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './root.css';
+import Home from './home/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
