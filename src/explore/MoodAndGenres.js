9cd2909b64d6ca5653b1b8bbb78b92f2aa785c54
mhduusop <mhduusop@gmail.com>
2022-06-26 01:45:51 +0700

    Add MoodAndGenres

--- src/explore/MoodAndGenres.js
+++ src/explore/MoodAndGenres.js
@@ -7,96 +7,96 @@ class Mood extends React.Component{
                 <div className="row">
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Indie & Alternative</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Indie & Alternative</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Charts</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Dangdut</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Rock</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Energy Boosters</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Metal</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="row mt-3">
-                    <div className="col-lg-2">
-                        <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Indie & Alternative</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-2">
-                        <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Charts</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-2">
-                        <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-2">
-                        <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-2">
-                        <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-2">
-                        <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Folk & Acoustic</p>
                         </div>
                     </div>
                 </div>
                 <div className="row mt-3">
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Indie & Alternative</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">2010s</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Charts</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">2000s</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Mood & genres</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">R&B & Soul</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Hip-Hop</p>
                         </div>
                     </div>
                     <div className="col-lg-2">
                         <div class="card-body moodAndGenres d-flex p-2 rounded">
-                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold">Mood & genres</p>
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Mood & genres</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-lg-2">
+                        <div class="card-body moodAndGenres d-flex p-2 rounded">
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Soundtracks & Musicals</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-2">
+                        <div class="card-body moodAndGenres d-flex p-2 rounded">
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Charts</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-2">
+                        <div class="card-body moodAndGenres d-flex p-2 rounded">
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Mood & genres</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-2">
+                        <div class="card-body moodAndGenres d-flex p-2 rounded">
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Mood & genres</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-2">
+                        <div class="card-body moodAndGenres d-flex p-2 rounded">
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Mood & genres</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-2">
+                        <div class="card-body moodAndGenres d-flex p-2 rounded">
+                            <p class="card-title text-white text-bold mt-2 ms-3 fw-bold fs-6">Mood & genres</p>
                         </div>
                     </div>
                 </div>
